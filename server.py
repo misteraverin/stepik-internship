79dfddee5998b854a316f8adc42bb2f63b0beda1
Maxim Averin <amax0703@gmail.com>
2017-05-14 22:40:37 +0300

    add cache

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from stepik_api import stepik_api
 
 
-cache = SimpleCache()
+simple_cache = SimpleCache()
 app = Flask(__name__)
 stepik = stepik_api()
 
@@ -24,7 +24,22 @@ def lessons(lesson_id):
         response = jsonify(status="Error", message="Lesson ID: "+lesson_id + " is less than zero.")
         response.status_code = 404
         return response
-    return jsonify({"ids": stepik.get_steps_id(lesson_id)})
+
+    try:
+        date = stepik.get_lesson_date(lesson_id)
+    except Eception as e:
+        response = jsonify(status="Error", message="Invalid Lesson ID: "+lesson_id)
+        response.status_code = 404
+        return response
+
+    cached = cache.get(str(lesson_id))
+
+    if not cached or cached[0] != date:
+        steps = stepik.get_steps_id(lesson_id)
+        cache.set(str(lesson_id), (date, steps))
+    else:
+        steps = cached[1]
+    return jsonify(steps)
 
 def main():
     app.run(threaded=True)
