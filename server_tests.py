3a82df5a8843fe2945fbdf6532cbfdaf5085533a
Maxim Averin <amax0703@gmail.com>
2017-05-14 23:55:33 +0300

    add server_tests

--- server_tests.py
+++ server_tests.py
@@ -1,20 +1,22 @@
 import unittest
+from stepik_api import stepik_api
 
-class TestStringMethods(unittest.TestCase):
+class TestStepikApi(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def setUp(self):
+        self.stepik = stepik_api()
+    def test_get_lesson_date(self):
+        lesson_dates = {"1": "2017-01-16T17:58:55Z", "300": "2016-09-13T03:47:58Z"}
+        for lesson in lesson_dates.keys():
+            requested_date = self.stepik.get_lesson_date(lesson)
+            self.assertEqual(requested_date, lesson_dates[lesson])
+    def test_fetch_object(self):
+        lesson = self.stepik.fetch_object("lesson", 300)
+        self.assertEqual(lesson['lessons'][0]['id'], 300)
+    def test_get_steps_id(self):
+        right_text_steps = [580, 578, 576, 448]
+        requested_steps = self.stepik.get_steps_id(15)
+        self.assertEqual(right_text_steps, requested_steps)
 
 if __name__ == '__main__':
     unittest.main()
